6520db90271e32c6715e81d2fcbfbbe1f40dbcf5
yinshawnrao <yinshawnrao@tencent.com>
2020-04-30 20:04:47 +0800

    antd init

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Layout, Menu } from 'antd';
+
+const { Header, Content, Footer } = Layout;
+
+function App() {
+    return (
+        <Layout className="layout">
+            <Header>
+                <div className="logo" />
+                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+                    <Menu.Item key="1">nav 1</Menu.Item>
+                    <Menu.Item key="2">nav 2</Menu.Item>
+                    <Menu.Item key="3">nav 3</Menu.Item>
+                </Menu>
+            </Header>
+            <Content style={{ padding: '0 50px' }}>
+                <div className="site-layout-content">Content</div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+        </Layout>
+    );
+}
+
+ReactDOM.render(<App />, document.querySelector('#app'));
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        hot: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: '前端存储桶',
+            scriptLoading: 'defer',
+            template: './src/index.html',
+            hash: true
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ]
+}
